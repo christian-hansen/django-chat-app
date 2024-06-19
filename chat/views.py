a757fdcf945450e70ed71f6ce95f8eb4b7306044
Christian Hansen <mail@hansen-net.de>
2024-06-19 11:17:32 +0200

    login redirect

--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,13 @@
 from django.http.response import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login
 from .models import Message, Chat
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/login/')
 def index(request):
+    
     if request.method == 'POST':        
         print("Request method is post")      
         print(request.POST['textmessage'])   
@@ -14,11 +17,12 @@ def index(request):
     return render(request, 'chat/index.html', {'chat_messages': chat_messages})
 
 def login_view(request):
+    redirect = request.GET.get('next')
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
         if user is not None:
             login(request, user)
-            return HttpResponseRedirect ('/chat/')
+            return HttpResponseRedirect (request.POST.get('redirect'))
         else:
-            return render(request, 'auth/login.html', {'wrongPassword': True})
-    return render(request, 'auth/login.html', {'wrongPassword': False})
+            return render(request, 'auth/login.html', {'wrongPassword': True}, {'redirect': redirect})
+    return render(request, 'auth/login.html', {'redirect': redirect})
